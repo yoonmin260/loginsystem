b09fe4049d2cabd832efd3ef69388845081f1e89
itg <itg@DESKTOP-83C6MKE>
2025-01-16 14:41:07 +0900

    test

--- dataset.js
+++ dataset.js
@@ -26,7 +26,7 @@ class DataSet{
 
 let d1 = new DataSet("개발환경구축")//메인 타이틀 //메뉴번호
 d1.set_content("사용자의 로그인 기능을 구현하여 로그인 사용자와 비로그인 사용자에 제공 서비스를 구분한다.")//서브 타이틀
-d1.set_img(0,{imgtitle:"테스트용 이미지",imgurl:"https://drive.google.com/file/d/1NXPYGbGZHcReeObo66VbO6OC_yKTkso_/view?usp=drive_link",imglog:"테스트를 위해 이미지를 업로드 해보았습니다.",sourceurl:"https://docs.google.com/document/d/1SHiGBY02on7wvdjpafNkhMzaKbU3--ey-fVaGWsJXao/edit?usp=drive_link"})//이미지타이틀
+d1.set_img(0,{imgtitle:"테스트용 이미지",imgurl:"화면 캡처 2025-01-16 143924.png",imglog:"테스트를 위해 이미지를 업로드 해보았습니다.",sourceurl:"https://docs.google.com/document/d/1SHiGBY02on7wvdjpafNkhMzaKbU3--ey-fVaGWsJXao/edit?usp=drive_link"})//이미지타이틀
 
 
 d1.set_content("목표 시스템 개발을 위한 적정한 언어를 선정하고 라이선스와 향후 지원사항을 참고하여 결정한다.")
