786ef5edbdbfba294745211136db0fb9bf7f011e
itg <itg@DESKTOP-83C6MKE>
2025-01-31 18:05:29 +0900

    1

--- dataset.js
+++ dataset.js
@@ -27,7 +27,7 @@ class DataSet{
 
 let d1 = new DataSet("개발환경구축")//메인 타이틀 //메뉴번호
 d1.set_content("사용자의 로그인 기능을 구현하여 로그인 사용자와 비로그인 사용자에 제공 서비스를 구분한다.")//서브 타이틀
-d1.set_img(0,{imgtitle:"요구사항명세서작성",imgurl:"https://drive.google.com/file/d/1rPzsPA1xgsVNUL8jVfQlEgIN6aG_n0Qp/view?usp=drive_link",imglog:"작성된 요구사항 명",sourceurl:"https://docs.google.com/document/d/1fYO-WllL1nq8-qJC6Aj8fTeQplv7hvQzxUfRQAgAamo/edit?usp=drive_link"})//이미지타이틀
+d1.set_img(0,{imgtitle:"요구사항명세서작성",imgurl:"https://drive.google.com/file/d/1rPzsPA1xgsVNUL8jVfQlEgIN6aG_n0Qp/view?usp=drive_link",imglog:"작성된 요구사항 명",sourceurl:"https://github.com/yoonmin260/loginsystem"})//이미지타이틀
 
 
 d1.set_content("목표 시스템 개발을 위한 적정한 언어를 선정하고 라이선스와 향후 지원사항을 참고하여 결정한다.")
